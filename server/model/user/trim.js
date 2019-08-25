503d59ed3a1967195a89d85ce1d4bafa1fdcaca3
gaurav <sharma02gaurav@gmail.com>
2019-08-26 00:45:25 +0530

    dynamic request and error handling

--- server/model/user/trim.js
+++ server/model/user/trim.js
@@ -7,14 +7,17 @@ import FFMPEGCommand from 'fluent-ffmpeg';
  * @author gaurav
  * @since 25 August, 2019 23:45:28
 */
-export default ({ id }) => new Promise(async (resolve, reject) => {
+export default ({ id, xPoint, yPoint, timeDiff }) => new Promise(async (resolve, reject) => {
 	try {
-		const ffmpeg = FFMPEGCommand(path.resolve(__dirname, 'video.mp4'));
-		// ffmpeg.seekInput('0:10').seek('0:10').saveToFile('converted.mp4');
-		const filename = `converted-${new Date().getTime()}`;
-		ffmpeg.setStartTime('0:10').setDuration(6).saveToFile(`${filename}.mp4`);
-		return resolve(ResponseUtility.SUCCESS({ data: filename }));
-		// write your code here.....
+		console.log(xPoint, yPoint, timeDiff);
+		if (xPoint && timeDiff) {
+			const ffmpeg = FFMPEGCommand(path.resolve(__dirname, 'video.mp4'));
+			// ffmpeg.seekInput('0:10').seek('0:10').saveToFile('converted.mp4');
+			const filename = `converted-${new Date().getTime()}`;
+			ffmpeg.setStartTime(xPoint).setDuration(timeDiff).saveToFile(`${filename}.mp4`);
+			return resolve(ResponseUtility.SUCCESS({ data: filename }));
+		} 
+		return reject(ResponseUtility.GENERIC_ERR({ message: 'Missing required properties.' }));
 	} catch (err) {
 		return reject(ResponseUtility.GENERIC_ERR({ message: 'Error', error: err.message }));
 	}
